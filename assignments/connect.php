baf8cbe19cf6e92222735deb2bde15b2e79acc85
itsmordecai <mordecai.kipngetich@gmail.com>
2022-03-03 10:07:05 +0300

    remove hospital admission to own repo

--- assignments/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$link = mysqli_connect("localhost", "root", "", "vivaldi");
-
-if ($link == true) {
-  echo "";
-} else {
-  echo "Error connecting " . mysqli_connect_error($link) . "<br>";
-}
